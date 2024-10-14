e5ddf73d159dcd007ae3c68711a2903f32ac3c51
dallen72 <12113373+dallen72@users.noreply.github.com>
2024-10-14 13:00:14 -0400

    able to move pan under ball, for the most part

--- node_3d.gd
+++ node_3d.gd
@@ -11,12 +11,17 @@ var random_num_generator
 var _previous_rotation_x
 var _previous_rotation_z
 var _pan_is_being_flipped
+var _pan_was_just_reset
+var _pan_is_being_moved_under_ball
+var Ball = preload("res://ball.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_initialize_auto_pan_rotate()
-		
+	
+	_pan_was_just_reset = false	
 	_pan_is_being_flipped = false
+	_pan_is_being_moved_under_ball = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -28,13 +33,36 @@ func _physics_process(delta: float) -> void:
 		rotate_pan(_angle_x, _angle_z)
 		
 	if (Input.is_action_just_pressed("ui_accept")):
-		flip_pan()
-		
-	if (Input.is_action_pressed("ui_accept")):
 		$panCalibrateTimer.start()
+
+
+	if (Input.is_action_just_released("ui_accept")):
+		_pan_was_just_reset = false
+		flip_pan()
+		$panCalibrateTimer.stop()
 
 	if (_pan_is_being_flipped == true):
 		continue_flipping_pan(_angle_x, _angle_z)
+
+	if (get_node_or_null("rig/ball")):
+		if ( ! _pan_is_rotating):
+			if (_pan_is_being_moved_under_ball):
+				$rig/ball.gravity_scale = 1
+				_pan_is_being_moved_under_ball = false
+				$rig.velocity = Vector3()
+		
+		if (_pan_is_being_moved_under_ball == true):
+			var direction_to_center = $rig/ball.position - Vector3(0, 0, -0.5) - $rig/ballSpawn.position
+			$rig.velocity = direction_to_center
+			print("debug, pan velocity: " + str($rig.velocity))
+			$rig.move_and_slide()
+
+		if ($rig/ball.position.length() > 3.0):
+			$rig/ball.queue_free()
+			await $rig/ball.tree_exited
+			var ball = Ball.instantiate()
+			$rig.add_child(ball)
+			ball.name = "ball"
 
 
 func _initialize_auto_pan_rotate():
@@ -53,37 +81,38 @@ func _initialize_auto_pan_rotate():
 
 
 func flip_pan():
-	_previous_rotation_x = $pan.rotation.x
-	_previous_rotation_z = $pan.rotation.z
-	_angle_z = -1 * $pan.rotation.z + random_num_generator.randf_range(-0.1, 0.1)
-	_angle_x = -1 * $pan.rotation.x + random_num_generator.randf_range(-0.1, 0.1)
+	_previous_rotation_x = $rig/pan.rotation.x
+	_previous_rotation_z = $rig/pan.rotation.z
+	_angle_z = -1 * $rig/pan.rotation.z + random_num_generator.randf_range(-0.1, 0.1)
+	_angle_x = -1 * $rig/pan.rotation.x + random_num_generator.randf_range(-0.1, 0.1)
 	_elapsed = 0.01
 	_pan_is_being_flipped = true
 
 
 func continue_flipping_pan(angle_x, angle_z):
-	var starting_angle_z = $pan.rotation.z
-	var starting_angle_x = $pan.rotation.x
-	$pan.rotation = Vector3(lerp_angle(starting_angle_x, angle_x, _elapsed), 0, lerp_angle(starting_angle_z, angle_z, _elapsed))
+	var starting_angle_z = $rig/pan.rotation.z
+	var starting_angle_x = $rig/pan.rotation.x
+	$rig/pan.rotation = Vector3(lerp_angle(starting_angle_x, angle_x, _elapsed), 0, lerp_angle(starting_angle_z, angle_z, _elapsed))
 	_elapsed += 0.1
 	var x_is_reached = false
 	var z_is_reached = false
-	if (_previous_rotation_x <= angle_x and $pan.rotation.x >= angle_x):
+	if (_previous_rotation_x <= angle_x and $rig/pan.rotation.x >= angle_x):
 		x_is_reached = true
-	elif (_previous_rotation_x >= angle_x and $pan.rotation.x <= angle_x):
+	elif (_previous_rotation_x >= angle_x and $rig/pan.rotation.x <= angle_x):
 		x_is_reached = true
-	if (_previous_rotation_z <= angle_z and $pan.rotation.z >= angle_z):
+	if (_previous_rotation_z <= angle_z and $rig/pan.rotation.z >= angle_z):
 		z_is_reached = true
-	elif (_previous_rotation_z >= angle_z and $pan.rotation.z <= angle_z):
+	elif (_previous_rotation_z >= angle_z and $rig/pan.rotation.z <= angle_z):
 		z_is_reached = true
 	elif (
-		abs($pan.rotation.z) > MAX_ANGLE
-		or abs($pan.rotation.x) > MAX_ANGLE):
+		abs($rig/pan.rotation.z) > MAX_ANGLE
+		or abs($rig/pan.rotation.x) > MAX_ANGLE):
 		_pan_is_being_flipped = false
 
 	if (x_is_reached and z_is_reached):
 		_pan_is_being_flipped = false
 
+	
 #	var target_angle = $pan.global_position.angle_to_point(target)
 	#$pan.rotation = Vector3(0, 0, 0) # x between pi/6 and -pi/6
 	#$pan.rotation = Vector3(0, 0, 0) # x and z between pi/6 and -pi/6
@@ -93,21 +122,21 @@ func continue_flipping_pan(angle_x, angle_z):
 
 
 func rotate_pan(angle_x, angle_z):
-	var starting_angle_z = $pan.rotation.z
-	var starting_angle_x = $pan.rotation.x
-	$pan.rotation = Vector3(lerp_angle(starting_angle_x, angle_x, _elapsed), 0, lerp_angle(starting_angle_z, angle_z, _elapsed))
+	var starting_angle_z = $rig/pan.rotation.z
+	var starting_angle_x = $rig/pan.rotation.x
+	$rig/pan.rotation = Vector3(lerp_angle(starting_angle_x, angle_x, _elapsed), 0, lerp_angle(starting_angle_z, angle_z, _elapsed))
 	_elapsed += 0.1
-	if (_previous_rotation_x < angle_x and $pan.rotation.x >= angle_x):
+	if (_previous_rotation_x < angle_x and $rig/pan.rotation.x >= angle_x):
 		_pan_is_rotating = false
-	elif (_previous_rotation_x > angle_x and $pan.rotation.x <= angle_x):
+	elif (_previous_rotation_x > angle_x and $rig/pan.rotation.x <= angle_x):
 		_pan_is_rotating = false
-	if (_previous_rotation_z < angle_z and $pan.rotation.z >= angle_z):
+	if (_previous_rotation_z < angle_z and $rig/pan.rotation.z >= angle_z):
 		_pan_is_rotating = false
-	elif (_previous_rotation_x > angle_x and $pan.rotation.z <= angle_z):
+	elif (_previous_rotation_x > angle_x and $rig/pan.rotation.z <= angle_z):
 		_pan_is_rotating = false
 	elif (
-		abs($pan.rotation.z) > MAX_ANGLE
-		or abs($pan.rotation.x) > MAX_ANGLE):
+		abs($rig/pan.rotation.z) > MAX_ANGLE
+		or abs($rig/pan.rotation.x) > MAX_ANGLE):
 		_pan_is_rotating = false
 
 
@@ -116,9 +145,13 @@ func _on_pan_rotation_timer_timeout() -> void:
 
 
 func _on_pan_calibrate_timer_timeout() -> void:
-	if 3 seconds passed:
-		_move_pan_under_ball()
+	_move_pan_under_ball()
+	_pan_was_just_reset = true
+	_pan_is_being_moved_under_ball = true
 		
 		
+# move the pan under the ball to like you would if you were balacing a ping pong paddle
 func _move_pan_under_ball():
-	# move the pan under the ball to like you would if you were balacing a ping pong paddle
+	$rig/ball.apply_impulse(-2*($rig/ball.position - $rig/ballSpawn.position))
+	$rig/ball.inertia = Vector3(0.1, 0.1, 0.1)
+	print("debug, pan moved under ball")
